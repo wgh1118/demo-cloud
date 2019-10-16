b9b7f9cffd4d80230173373ef3de3ab055aeb1a7
wanggh <hugh_wanggh@163.com>
2019-10-16 18:05:27 +0800

    oauth2

--- authorization/src/main/java/com/wanggh/cloud/authorization/AuthorizationApp.java
+++ authorization/src/main/java/com/wanggh/cloud/authorization/AuthorizationApp.java
@@ -3,8 +3,12 @@ package com.wanggh.cloud.authorization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableDiscoveryClient
+@EnableAuthorizationServer
+@EnableFeignClients("com.wanggh.cloud.authorization.rpc")
 @SpringBootApplication
 public class AuthorizationApp {
     public static void main(String[] args) {
